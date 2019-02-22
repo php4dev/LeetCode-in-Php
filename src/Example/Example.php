8e9a62f07af2ede62fe6de477ed1eaffec9bb21c
Valentyn Kolesnikov <javadev75@gmail.com>
2019-02-22 15:52:44 +0100

    Create Example.php

---
+++ src/Example/Example.php
@@ -0,0 +1,12 @@
+<?php namespace Example;
+class Example
+{
+    public static function go()
+    {
+        if (false) {
+            return true;
+        }
+        return false;
+        
+    }
+}
