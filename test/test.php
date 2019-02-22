e1ab607fdb6d680780ab0f385bb1f2168498032a
Valentyn Kolesnikov <javadev75@gmail.com>
2019-02-22 15:53:24 +0100

    Create test.php

---
+++ test/test.php
@@ -0,0 +1,10 @@
+<?php
+    require __DIR__ .'/../vendor/autoload.php';
+    class Test extends PHPUnit_Framework_TestCase
+    {
+        public function testExample()
+        {
+            $result = Example\Example::go();
+        }
+    }
+?>  
