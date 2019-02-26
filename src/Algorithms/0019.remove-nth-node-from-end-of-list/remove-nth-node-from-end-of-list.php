6ecba97a4d5386c55767ec01fe75060a656b6524
Valentyn Kolesnikov <javadev75@gmail.com>
2019-02-26 15:38:46 +0100

    Add solution 19.

---
+++ src/Algorithms/0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.php
@@ -0,0 +1,47 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @param Integer $n
+     * @return ListNode
+     */
+    function removeNthFromEnd($head, $n) {
+        $list = [];
+        $size = 1;
+        $a = $head;
+        
+        while($a->next != null){
+            $list[] = $a;
+            $a=$a->next;
+            $size++;
+        }
+        $list[] = $a;
+        
+        $c = null;
+        
+        if($size==1){            
+            return $c;
+        }
+        if($size==$n){
+            $head=$head->next;
+            return $head;
+        }
+        
+        $b=$list[$size-$n-1];
+        
+        if($b->next->next == null)
+            $b->next=null;
+        else
+            $b->next=$b->next->next;
+        
+        return $head;
+    }
+}
