bf8fad8580293ca85193875897cd7479b876f2e5
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-04 08:40:05 +0200

    Add solution 48.

---
+++ src/Algorithms/0048.rotate-image/rotate-image.php
@@ -0,0 +1,28 @@
+class Solution {
+
+    /**
+     * @param Integer[][] $matrix
+     * @return NULL
+     */
+    function rotate(&$matrix) {
+        for ($i = 0; $i < count($matrix); $i++) {
+            for ($j = $i; $j < count($matrix); $j++) {
+                if ($i == $j) continue;
+                $temp = $matrix[$i][$j];
+                $matrix[$i][$j] = $matrix[$j][$i];
+                $matrix[$j][$i] = $temp;
+            }
+        }
+        for ($i = 0; $i < count($matrix); $i++) {
+            $start  = 0;
+            $end = count($matrix[$i]) - 1;
+            while ($start < $end) {
+                $temp = $matrix[$i][$start];
+                $matrix[$i][$start] = $matrix[$i][$end];
+                $matrix[$i][$end] = $temp;
+                $start++;
+                $end--;
+            }
+        } 
+    }
+}
