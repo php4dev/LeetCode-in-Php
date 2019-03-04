3e95a1633ecd7e9ca82ea1b2c17e1616d2551a5f
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-04 11:24:36 +0200

    Add solution 58.

---
+++ src/Algorithms/0058.length-of-last-word/length-of-last-word.php
@@ -0,0 +1,20 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function lengthOfLastWord($s) {
+        $length = 0;
+        $first = false;
+        for ($i = strlen($s) - 1; $i >= 0; $i--) {
+            if ($s[$i] == ' ' && $first) {
+                return $length;
+            } else if ($s[$i] != ' ') {
+                $first = true;
+                $length++;
+            }
+        }
+        return $length;
+    }
+}
