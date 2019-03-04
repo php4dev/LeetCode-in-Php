3e9ff53102b3977ddf14e2bee2e1b8c548ef8458
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-04 11:22:33 +0200

    Add solution 57.

---
+++ src/Algorithms/0057.insert-interval/insert-interval.php
@@ -0,0 +1,44 @@
+/**
+ * Definition for an interval.
+ * class Interval {
+ *     public $start = 0;
+ *     public $end = 0;
+ *     function __construct(int $start = 0, int $end = 0) {
+ *         $this->start = $start;
+ *         $this->end = $end;
+ *     }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Interval[] $intervals
+     * @param Interval $newInterval
+     * @return Interval[]
+     */
+    function insert($intervals, $newInterval) {
+        $res = [];
+        
+        $s = $newInterval->start;
+        $e = $newInterval->end;
+        
+        foreach($intervals as $i) {
+            if($i->start > $e) {
+                array_push($res, new Interval($s,$e));
+                $s = $i->start;
+                $e = $i->end;
+            }
+            
+            if($s <= $i->end) {
+                $s = min($s, $i->start);
+                $e = max($e, $i->end);
+            }
+            else {
+                array_push($res, $i);
+            }
+        }
+        
+        array_push($res, new Interval($s,$e));
+        return $res; 
+    }
+}
