9fcaf9f009ef59f532a60bb789d0ea93eeef9cf9
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-04 09:14:55 +0200

    Add solution 50.

---
+++ src/Algorithms/0050.powx-n/powx-n.php
@@ -0,0 +1,44 @@
+class Solution {
+
+    /**
+     * @param Float $x
+     * @param Integer $n
+     * @return Float
+     */
+    function myPow($x, $n) {
+        if($x == 1) {
+            return $x;
+        }
+        if($x == -1) {
+            return $n % 2 == 0 ? 1 : -1;
+        }
+        if($n == 0) {
+            return 1;
+        }
+        if($n < 0) {
+            $x = 1/$x;
+            $n = abs($n);
+        }
+        return self::helper($x, $n);
+    }
+    
+    function helper($x, $n) {
+        if ($x == 0) {
+            return 0;
+        }
+        if ($n == 1) {
+            return $x;
+        }
+        if ($n == 0) {
+            return 1;
+        }
+        
+        $output = self::helper($x, $n/2);
+        
+        if($n % 2 == 0) {
+            return $output * $output;
+        }else {
+            return $output * $output * $x;
+        }
+    }
+}
