8ed01e0621bd4447e00ee24c8d27edc568f6a6dc
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-04 09:05:31 +0200

    Add solution 49.

---
+++ src/Algorithms/0049.group-anagrams/group-anagrams.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param String[] $strs
+     * @return String[][]
+     */
+    function groupAnagrams($strs) {
+        $map = [];
+        for($i = 0; $i < count($strs); $i++) {
+            $s = $strs[$i];
+            $temp = str_split($s);
+            sort($temp);
+            $sorted = implode($temp);
+            if($map[$sorted] == null) {
+                $map[$sorted] = [];
+            }
+            array_push($map[$sorted], $strs[$i]);
+        }
+        return array_values($map);
+    }
+}
