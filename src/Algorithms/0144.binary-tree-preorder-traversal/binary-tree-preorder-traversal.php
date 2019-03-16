ab5feeaab2e9355949059871a4166338ae537dfc
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-16 17:42:07 +0200

    Add solution 144.

---
+++ src/Algorithms/0144.binary-tree-preorder-traversal/binary-tree-preorder-traversal.php
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Integer[]
+     */
+    function preorderTraversal($root) {
+        $list = [];
+        $stack = [];
+        array_push($stack, $root);
+        while(!empty($stack)){
+            $node = array_pop($stack);
+            while($node != null){
+                array_push($list, $node->val);
+                array_push($stack, $node->right);
+                $node = $node->left;
+            }            
+        }
+        return $list;
+    }
+}
