78e0067bd433cc4d5058db14baa3b186367407b8
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-17 07:25:50 +0200

    Add solution 148.

---
+++ src/Algorithms/0148.sort-list/sort-list.php
@@ -0,0 +1,50 @@
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val) { $this->val = $val; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param ListNode $head
+     * @return ListNode
+     */
+    function sortList($head) {
+        if ($head == null || $head->next == null)
+            return $head;
+        $slow = $head;
+        $fast = $head;
+        $pre = $slow;
+        while($fast != null && $fast->next != null) {
+            $pre = $slow;
+            $slow = $slow->next;
+            $fast = $fast->next->next;
+        }
+        $pre->next = null;
+        $first = self::sortList($head);
+        $second = self::sortList($slow);
+        $res = new ListNode(1);
+        $cur = $res;
+        while ($first != null || $second != null) {
+            if ($first == null) {
+                $cur->next= $second;
+                break;
+            } else if ($second == null) {
+                $cur->next = $first;
+                break;
+            } else if ($first->val <= $second->val) {
+                $cur->next = $first;
+                $first = $first->next;
+                $cur = $cur->next;
+            } else {
+                $cur->next = $second;
+                $second = $second->next;
+                $cur = $cur->next;
+            }
+        }
+        return $res->next;
+    }
+}
