bc735b92fa1af1dd95ec3ccd062d0d0cfbf9a681
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-20 14:26:07 +0200

    Add solution 153.

---
+++ src/Algorithms/0153.find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.php
@@ -0,0 +1,14 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findMin($nums) {
+        for($i=1;$i<count($nums);$i++){
+            if($nums[$i]<$nums[$i-1])
+                return $nums[$i];
+        }
+        return $nums[0];
+    }
+}
