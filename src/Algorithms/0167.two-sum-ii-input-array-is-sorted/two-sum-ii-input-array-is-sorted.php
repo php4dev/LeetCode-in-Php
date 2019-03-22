308793f1fe780245f77418aa03f247011e059c07
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-22 12:46:09 +0200

    Add solution 167.

---
+++ src/Algorithms/0167.two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.php
@@ -0,0 +1,21 @@
+class Solution {
+
+    /**
+     * @param Integer[] $numbers
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($numbers, $target) {
+        $res=array_fill(0, 2, 0);
+        for ($i=0,$j=count($numbers)-1;$i<$j;){
+            $sum = $numbers[$i]+$numbers[$j];
+            if ($sum==$target){
+                $res[0]=$i+1; $res[1]=$j+1;
+                return $res;
+            }
+            else if ($sum<$target)$i++;
+            else $j--;
+        }
+        return $res;
+    }
+}
