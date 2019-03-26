3997671cc6ffbb25ebcc9285a973caf3a11b6376
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-26 21:05:14 +0200

    Add solution 169.

---
+++ src/Algorithms/0169.majority-element/majority-element.php
@@ -0,0 +1,11 @@
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function majorityElement($nums) {
+        sort($nums);
+        return $nums[intval(count($nums)/2)];
+    }
+}
