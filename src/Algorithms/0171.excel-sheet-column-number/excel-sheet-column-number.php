3356ee96c8b44f46079d41c9edd584405f9db031
Valentyn Kolesnikov <javadev75@gmail.com>
2019-03-31 17:58:10 +0300

    Add solution 171.

---
+++ src/Algorithms/0171.excel-sheet-column-number/excel-sheet-column-number.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Integer
+     */
+    function titleToNumber($s) {
+        $len=strlen($s);
+        $jinzhi=1;
+        $sum=0;
+        for($i=$len-1;$i>=0;$i--)
+        {
+            
+            $sum=$sum+(ord($s[$i])-64)*$jinzhi;
+            $jinzhi=$jinzhi*26;
+        }
+        return $sum;
+    }
+}
