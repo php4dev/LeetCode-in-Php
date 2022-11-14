4a4a1df6fb8b8f242c799a8b51732fe16ce32804
Reza Rabbani <50789773+thrashzone13@users.noreply.github.com>
2022-11-14 05:51:54 +0100

    Added task 771.

---
+++ src/Algorithms/0771.jewels-and-stones/jewels-and-stones.php
@@ -0,0 +1,15 @@
+class Solution {
+
+    /**
+    * @param String $jewels
+    * @param String $stones
+    * @return Integer
+    */
+    function numJewelsInStones($jewels, $stones) {
+        $jewelsArr = str_split($jewels);
+        foreach($jewelsArr as $jewel) {
+            $b += substr_count($stones, $jewel);
+        }
+        return $b;
+    }
+}
