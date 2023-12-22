a5d0796c7ffaf6025846d519b6519a504e25795a
Valentyn Kolesnikov <javadev75@gmail.com>
2023-12-22 04:14:26 +0200

    Fixed test

---
+++ tests/Solution.php
@@ -0,0 +1,23 @@
+<?php
+
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @param Integer $target
+     * @return Integer[]
+     */
+    function twoSum($nums, $target) {
+        $map = [];
+        for ($i = 0; $i < count($nums); $i++) {
+            $map[$nums[$i]] = $i;
+        }
+        for ($i = 0; $i < count($nums); $i++) {
+            $complement = $target - $nums[$i];
+            if (array_key_exists($complement, $map) && $map[$complement] != $i) {
+                return [$i, $map[$complement] ];
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");      
+    }
+}
